1919f89e0ddde2a01d8c04053ec39163fc827cdb
tonyWh1te <zmyshenko.valery@bk.ru>
2023-05-06 00:32:44 +0300

    adding a static homepage

--- src/views/Home/HomePage/HomePage.js
+++ src/views/Home/HomePage/HomePage.js
@@ -1,7 +1,214 @@
+import { useState, useRef, useEffect } from 'react';
+import { home, library, profilePic, coverPic1, coverPic2, coverPic3, artistPic } from '../../../assets';
 import './HomePage.css';
 
 function HomePage() {
-  return '';
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef();
+  const arrowRef = useRef();
+
+  const toggleDropdown = (e) => {
+    if (e.target !== menuRef.current && e.target !== arrowRef.current) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', toggleDropdown);
+
+    return () => {
+      window.removeEventListener('click', toggleDropdown);
+    };
+  });
+
+  return (
+    <div className="relative">
+      <div className="nav">
+        <nav className="nav__inner">
+          <ul className="nav__list">
+            <li className="nav__list-item nav__item--active">
+              <a className="nav__link nav__link--active" href="#">
+                <img className="nav__img" src={home} alt="nav-icon" />
+                <span className="nav__name">Home</span>
+              </a>
+            </li>
+            <li className="nav__list-item">
+              <a className="nav__link" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="nav__img">
+                  <path
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="nav__name">Browse</span>
+              </a>
+            </li>
+            <li className="nav__list-item">
+              <a className="nav__link" href="#">
+                <img className="nav__img" src={library} alt="nav-icon" />
+                <span className="nav__name">Library</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="absolute left-0 right-0 md:left-56">
+        <header>
+          <div className="container px-12 mx-auto md:mx-0">
+            <div className="header__inner">
+              <div className="flex justify-between items-center">
+                <div className="relative">
+                  <input className="search__input" type="text" placeholder="Search" />
+                  <div className="search__img" data-search>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-4 h-4 stroke-1 stroke-gray-icon text-gray-icon md:stroke-black md:text-black"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <div className="search__close" data-close>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 text-black">
+                      <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                    </svg>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <div>
+                    <img className="user__img" src={profilePic} alt="profile-pic" />
+                  </div>
+                  <div className="user__text">Tiffany Kim</div>
+                  <div className="cursor-pointer" onClick={() => setOpen(!open)} ref={arrowRef}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-5 h-5 text-gray-icon pointer-events-none"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              {open && (
+                <div className="dropdown-menu" ref={menuRef} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+                  <div className="bg-gray-gradient-dark shadow-dropdown rounded-md">
+                    <div className="py-1" role="none">
+                      <a href="#" className="dropdown-menu__link animation-main" role="menuitem" tabIndex="-1" id="menu-item-0">
+                        Account settings
+                      </a>
+                      <form method="POST" action="#" role="none">
+                        <button type="submit" className="dropdown-menu__btn animation-main" role="menuitem" tabIndex="-1" id="menu-item-3">
+                          Sign out
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </header>
+        <main>
+          <div className="container px-12 mx-auto md:mx-0">
+            <h1 className="page-title">Home</h1>
+          </div>
+          <section className="pb-14">
+            <div className="container px-12 mx-auto md:mx-0">
+              <h2 className="section-title">Popular songs</h2>
+              <ul className="songs-list">
+                <li className="card-song">
+                  <img className="card-song__cover" src={coverPic1} alt="cover" />
+                  <h4 className="card-title">LukeXXy</h4>
+                  <p className="card-song__singer">The Vegabonds</p>
+                </li>
+                <li className="card-song">
+                  <img className="card-song__cover" src={coverPic2} alt="cover" />
+                  <h4 className="card-title">Cameron Williamson</h4>
+                  <p className="card-song__singer">The Vegabonds</p>
+                </li>
+                <li className="card-song">
+                  <img className="card-song__cover" src={coverPic3} alt="cover" />
+                  <h4 className="card-title">RonaldRich</h4>
+                  <p className="card-song__singer">The Vegabonds</p>
+                </li>
+                <li className="card-song">
+                  <img className="card-song__cover" src={coverPic2} alt="cover" />
+                  <h4 className="card-title">CameronW</h4>
+                  <p className="card-song__singer">The Vegabonds</p>
+                </li>
+                <li className="card-song">
+                  <img className="card-song__cover" src={coverPic1} alt="cover" />
+                  <h4 className="card-title">JJDevon</h4>
+                  <p className="card-song__singer">The Vegabonds</p>
+                </li>
+                <li className="card-song">
+                  <img className="card-song__cover" src={coverPic1} alt="cover" />
+                  <h4 className="card-title">JJDevon</h4>
+                  <p className="card-song__singer">The Vegabonds</p>
+                </li>
+                <li className="card-song">
+                  <img className="card-song__cover" src={coverPic2} alt="cover" />
+                  <h4 className="card-title">CameronW</h4>
+                  <p className="card-song__singer">The Vegabonds</p>
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section className="pb-32 md:pb-14">
+            <div className="container px-12 mx-auto md:mx-0">
+              <h2 className="section-title">Popular artists</h2>
+              <ul className="artists-list">
+                <li className="card-artist">
+                  <img className="card-artist__img" src={artistPic} alt="artist" />
+                  <h4 className="card-title">David Bowie</h4>
+                </li>
+                <li className="card-artist">
+                  <img className="card-artist__img" src={artistPic} alt="artist" />
+                  <h4 className="card-title">David Bowie</h4>
+                </li>
+                <li className="card-artist">
+                  <img className="card-artist__img" src={artistPic} alt="artist" />
+                  <h4 className="card-title">David Bowie</h4>
+                </li>
+                <li className="card-artist">
+                  <img className="card-artist__img" src={artistPic} alt="artist" />
+                  <h4 className="card-title">David Bowie</h4>
+                </li>
+                <li className="card-artist">
+                  <img className="card-artist__img" src={artistPic} alt="artist" />
+                  <h4 className="card-title">David Bowie</h4>
+                </li>
+                <li className="card-artist">
+                  <img className="card-artist__img" src={artistPic} alt="artist" />
+                  <h4 className="card-title">David Bowie</h4>
+                </li>
+                <li className="card-artist">
+                  <img className="card-artist__img" src={artistPic} alt="artist" />
+                  <h4 className="card-title">David Bowie</h4>
+                </li>
+                <li className="card-artist">
+                  <img className="card-artist__img" src={artistPic} alt="artist" />
+                  <h4 className="card-title">David Bowie</h4>
+                </li>
+              </ul>
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,57 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    letterSpacing: {
+      tighter: '-.06em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.045em',
+      wider: '.063em',
+      widest: '.1em',
+    },
     screens: {
       sm: '480px',
       md: '768px',
-      lg: '992px',
+      lg: '1024px',
       xl: '1440px',
+      '2xl': '1696px',
     },
     fontFamily: {
       main: ['Inter', 'sans-serif'],
       'text-landing': ['Hurricane', 'cursive'],
     },
     extend: {
+      height: {
+        'nav-height': '80px',
+      },
+      gridTemplateColumns: {
+        songs: 'repeat(10, 1fr)',
+        artists: 'repeat(20, 1fr)',
+      },
+      dropShadow: {
+        'cover-pic': '0px 0px 10px rgba(0, 0, 0, 0.6)',
+      },
+      boxShadow: {
+        dropdown: '0px 5px 30px rgba(0, 0, 0, 0.8)',
+      },
       colors: {
         'main-green': '#1ED760',
         'form-green': '#1AB26B',
         'gray-line': '#4F4F4F',
-        'gray-input': '#4C4C4',
+        'gray-input': '#C4C4C4',
         'gray-sidebar': '#030303',
+        'gray-icon': '#ABABAB',
         'gray-gradient-light': '#282828',
         'gray-gradient-dark': '#121212',
       },
       backgroundImage: (theme) => ({
         'gradient-main': `linear-gradient(176.13deg, ${theme('colors.gray-gradient-light')} 2.6%, ${theme('colors.gray-gradient-dark')} 104.99%)`,
+        'gradient-dropdown': 'linear-gradient(180deg, rgba(23, 23, 23, 0) 0%, #000000 100%);',
       }),
     },
   },
-  plugins: [],
+  plugins: [plugin(function ({ addUtilities }) {})],
 };
